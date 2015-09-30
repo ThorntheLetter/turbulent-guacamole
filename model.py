2c84245fa5f12c2bfad279cc6808618b800581b1
ThorntheLetter <ThorntheLetter@users.norepy.github.com>
2015-09-30 18:44:40 -0500

    I think I added a comment

--- model.py
+++ model.py
@@ -3,3 +3,7 @@ import theano, theano.tensor as T
 import random
 import theano_lstm
 
+class Model:
+	"""Model to predict idk a square wave or something"""
+	def __init__(self, hiddenSize, inputSize):
+		
