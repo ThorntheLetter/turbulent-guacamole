8d2ec35d209bc82f1dbeb89166091809eb81baa7
ThorntheLetter <ThorntheLetter@users.norepy.github.com>
2015-10-09 14:14:27 -0500

    I learned keras exists.

--- model.py
+++ model.py
@@ -1,25 +1,41 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.recurrent import LSTM
 import numpy as np
-import theano, theano.tensor as T
-import random
-import theano_lstm
-from theano_lstm import LSTM, StackedCells, Layer
 
-class Model:
-	"""Model to predict idk a square wave or something, mostly based on the theano_lstm tutorial"""
-	def __init__(self, hiddenSize, inputSize, stackSize = 1, cellType = LSTM):
-		self.model = StackedCells(inputSize, cellType = cellType, layers =[hidden_size] * stack_size) #Most of model
-		self.model.layers.append(Layer(hiddenSize, inputSize, activation = np.tanh)) #Output layer, same size as input layer.
-		self.inputMat = T.imatrix() #The input matrix is integers, square wave can work with that, change for other data types
-		self.primingWord = T.iscalar() #not sure what this does yet so i am keeping it even though it looks like word stuff
-		self.srng = T.sharedRandomstreams.RandomStreams(np.random.randint(0, 1024)) #rng
-		self.predictions = self.createPrediction() #not sure why this is ana attribute and method but okay
-		self.greedyPredictions = self.createPrediction(greedy=True) #same, supposedly these are for symbolic variables.
-		self.create_cost_fun() #these 3 are for training
-		self.create_training_function()
-		self.create_predict_function()
 
-	@property
-	def params(self): #accessor for params
-		return self.model.params
+layer1_nodes = 256
+layer2_nodes = 256
+#I think I will do a sine wave
+model = Sequential()
+model.add(LSTM(1, return_sequences = True, output_dim = layer1_nodes))
+#Dropout goes here, but i wanted it to somewhat work before making it more complex.
+model.add(LSTM(layer1_nodes, return_sequences = False, output_dim = layer2_nodes))
+model.add(Dense(layer1_nodes, 1)) #The Output: layer2_nodes connections in, one connection out.
+model.add(Activation("sigmoid"))
 
-	
+model.compile(loss='mse', optimizer='rmsprop') #mse because I somewhat know how that one works, rmsprop because I haven't looked for good desctiptions of those yet.\
+print("done") #so that I know it is done. test 1 done in 174.8s. does adding gpu help for this? test 2 in 93.4s.
+
+#simple data set code that will get rewritten to show progress later.
+x = np.zeros((100,10,1))
+y = np.zeros((100,1))
+for i in range(100):
+	for j in range(10):
+		x[i,j,0] = (i + j) / 10
+	y[i,0] = (i + 11) / 11
+
+x = np.sin(x)
+y = np.sin(y) 
+
+for i in range(5):
+	print("iteration: ", i)
+	model.fit(x, y, batch_size = 1, verbose = 2)
+
+xseed = np.zeros((1,10,1))
+for i in range(10):
+	for j in range(10):
+		xseed[0,j,0] = i + (j / 10)
+	xseed = np.sin(xseed)
+	print("prediction: ", model.predict(xseed))
+	print("actual: ", np.sin(i + 1.1))
