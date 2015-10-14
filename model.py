ceb8ca5ff53458c1511985337d2bcd434351e26d
thorntheletter <thorntheletter@users.noreply.github.com>
2015-10-14 01:09:33 -0500

    fixed some errors found during testing

--- model.py
+++ model.py
@@ -10,10 +10,10 @@ LAYER1_SIZE = 256
 LAYER2_SIZE = 256
 DROPOUT_RATE = .3
 
-model = Sequential()
-model.add(LSTM(1, return_sequences = True, output_dim = LAYER1_SIZE))
-model.add(Dropout(DROPOUT_RATE))
-model.add(LSTM(LAYER1_SIZE, return_sequences = False, output_dim = LAYER2_SIZE))
-model.add(Dropout(DROPOUT_RATE))
-model.add(Dense(LAYER2_SIZE, 1))
-model.compile(loss='mse', optimizer='rmsprop') #mse because I somewhat know how that one works, rmsprop because I haven't looked for good desctiptions of those yet.
+themodel = Sequential()
+themodel.add(LSTM(1, return_sequences = True, output_dim = LAYER1_SIZE))
+themodel.add(Dropout(DROPOUT_RATE))
+themodel.add(LSTM(LAYER1_SIZE, return_sequences = False, output_dim = LAYER2_SIZE))
+themodel.add(Dropout(DROPOUT_RATE))
+themodel.add(Dense(LAYER2_SIZE, 1))
+themodel.compile(loss='mse', optimizer='rmsprop') #mse because I somewhat know how that one works, rmsprop because I haven't looked for good desctiptions of those yet.
--- waveloader.py
+++ waveloader.py
@@ -31,20 +31,20 @@ def arrange_samples(file, sample_length = DEFAULT_SAMPLE_LENGTH):
 
 
 def predict_samples(outfile, infile, length = 500000, sample_length = DEFAULT_SAMPLE_LENGTH):
-	outfile.setnchannels(1)
-	outfile.setframerate(infile.getframerate())
+	outfile.setparams(infile.getparams())
+
 	x = np.zeros((1,sample_length,1), dtype = int16)
 	for i in range(length):
-		y = model.predict(x)[0][0]
+		y = model.themodel.predict(x)[0][0]
 		x = np.delete(x,0,1)
 		x = np.append(x, [[[y]]], axis = 1)
-		outfile.writeframes(struct.pack(int(np.round(y))))
+		outfile.writeframes(struct.pack('h', int(np.round(y))))
 
 
 #currently will only support mono 16-bit signed int PCM wave files, but setting up to add more support later.
 if __name__ == "__main__":
 	infile = wave.open(sys.argv[1], 'rb')
-	outfile = wave.open(output.wav, 'w')
+	outfile = wave.open('output.wav', 'wb')
 	x, y = arrange_samples(infile)
-	model.fit(x, y, batch_size = 100, verobose = 2)
+	model.themodel.fit(x, y, batch_size = 100, verbose = 2)
 	predict_samples(outfile, infile)
