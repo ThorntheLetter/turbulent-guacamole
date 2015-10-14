a7778563ffc34eadcde0659658e8e89bfb2df0ed
ThorntheLetter <ThorntheLetter@users.norepy.github.com>
2015-10-13 22:25:00 -0500

    Added wave prediction function.

--- waveloader.py
+++ waveloader.py
@@ -2,7 +2,7 @@ import wave
 import struct
 import numpy as np
 import sys
-#import model
+import model
 
 DEFAULT_SAMPLE_LENGTH = 1000
 
@@ -30,7 +30,21 @@ def arrange_samples(file, sample_length = DEFAULT_SAMPLE_LENGTH):
 	return (x, y)
 
 
-#currently will only support mono 16-bit signed int PCM wave files
+def predict_samples(outfile, infile, length = 500000, sample_length = DEFAULT_SAMPLE_LENGTH):
+	outfile.setnchannels(1)
+	outfile.setframerate(infile.getframerate())
+	x = np.zeros((1,sample_length,1), dtype = int16)
+	for i in range(length):
+		y = model.predict(x)[0][0]
+		x = np.delete(x,0,1)
+		x = np.append(x, [[[y]]], axis = 1)
+		outfile.writeframes(struct.pack(int(np.round(y))))
+
+
+#currently will only support mono 16-bit signed int PCM wave files, but setting up to add more support later.
 if __name__ == "__main__":
 	infile = wave.open(sys.argv[1], 'rb')
-	arrange_samples(infile)
+	outfile = wave.open(output.wav, 'wb')
+	x, y = arrange_samples(infile)
+	model.fit(x, y, batch_size = 100, verobse = 0)
+	predict_samples(outfile, infile)
