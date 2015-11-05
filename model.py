f396596934f7aecac10095a6dbd43e1d1e134aeb
ThorntheLetter <ThorntheLetter@users.norepy.github.com>
2015-11-05 16:04:02 -0600

    updated keras model for new api

--- model.py
+++ model.py
@@ -10,9 +10,9 @@ LAYER2_SIZE = 256
 DROPOUT_RATE = .3
 
 themodel = Sequential()
-themodel.add(LSTM(1, return_sequences = True, output_dim = LAYER1_SIZE))
+themodel.add(LSTM(LAYER1_SIZE, return_sequences = True, input_dim = 1))
 themodel.add(Dropout(DROPOUT_RATE))
-themodel.add(LSTM(LAYER1_SIZE, return_sequences = False, output_dim = LAYER2_SIZE))
+themodel.add(LSTM(LAYER1_SIZE, return_sequences = False))
 themodel.add(Dropout(DROPOUT_RATE))
 themodel.add(Dense(LAYER2_SIZE, 1))
 themodel.compile(loss='mse', optimizer='rmsprop') #mse because I somewhat know how that one works, rmsprop because I haven't looked for good desctiptions of those yet.
